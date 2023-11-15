1b89050372fd1bb827708aa77ab59d0002769ffe
imm0807 <144823460+imm0807@users.noreply.github.com>
2023-11-15 08:17:37 -0500

    Update board size 2

--- sketch.js
+++ sketch.js
@@ -18,8 +18,8 @@ var world;
 var particles = [];
 var plinkos = [];
 var bounds = [];
-var cols = 11;
-var rows = 10;
+var cols = 111;
+var rows = 100;
 
 function preload() {
   ding = loadSound('ding.mp3');
