25986a5fcd901a35a6b3b075243bbe607eace9f4
imm0807 <144823460+imm0807@users.noreply.github.com>
2023-11-20 11:05:37 -0500

    Update board size 12

--- sketch.js
+++ sketch.js
@@ -28,7 +28,7 @@ function preload() {
 var paused = false;
 
 function setup() {
-  createCanvas(800, 700);
+  createCanvas(800, 600);
   colorMode(HSB);
   engine = Engine.create();
   world = engine.world;
